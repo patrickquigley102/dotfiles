b76db283420785ea787b3a809689c6535ad5f809
PQ <patrickquigley102@gmail.com>
2020-04-28 08:28:30 +0100

    Adding a function to flush memcache.

--- .bashfiles/2aliases_and_functions/kubernetes.sh
+++ .bashfiles/2aliases_and_functions/kubernetes.sh
@@ -24,3 +24,17 @@ kexec() {
   echo -e ${RED}"No context alias found for $1"${NOCOLOR}
 }
 
+# Clear TBG production memcached
+# Taken from: https://studentbeans.slab.com/posts/flushing-memcached-to-clear-cache-0mvb3omm
+# Assumes 3 pods named memcached-0 - memcached-2
+clear_memcached() {
+  for i in {0..2}; do 
+    kubectl -nmemcached port-forward memcached-$i "1121$i:11211" &
+  done
+  sleep 10 # to let it set up connections
+  for i in {0..2}; do 
+    echo 'flush_all' | nc localhost 1121$i -N
+  done
+  kill $(jobs -p)
+}
+

c6c8114eb0f1410a4aeea776991a1b62d2ce823e
PQ <patrickquigley102@gmail.com>
2020-04-28 09:07:18 +0100

    Add sb cli to PATH if missing.

--- .bashfiles/0env/tbg.sh
+++ .bashfiles/0env/tbg.sh
@@ -3,3 +3,7 @@ export TBG_GEMS_USER=vincent
 export SB_GIT_REMOTE_ROOT=git@git.thebeansgroup.com
 export DB_PASSWORD=password
 export SB_SRC_ROOT=$HOME/src/
+
+if ! echo $PATH | grep '\.sb' > /dev/null ; then
+  export PATH=$PATH:$HOME/.sb/dev-cli/bin
+fi

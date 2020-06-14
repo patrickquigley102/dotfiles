c93977d930edf2baed1a8b74249d14a616f7c9b7
Patrick Quigley <patrickquigley102@gmail.com>
2020-06-14 13:36:13 +0100

    Load sb cli aliases if present.

--- .bashfiles/2aliases_and_functions/general.sh
+++ .bashfiles/2aliases_and_functions/general.sh
@@ -7,10 +7,6 @@ fi
 alias ll='ls -alF'
 alias lr='ls -a -1 -R'
 
-# Add an "alert" alias for long running commands.  Use like so:
-# sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -       n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
 # Folding at home
 alias foldstop='sudo /etc/init.d/FAHClient stop'
 alias foldstart='sudo /etc/init.d/FAHClient start'
